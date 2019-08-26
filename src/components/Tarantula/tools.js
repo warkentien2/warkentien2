a32f0749798723f459ca4610864ebe8a2d603f13
philip15 <philip@growthhackers.com>
2019-08-26 03:14:14 -0300

    Reshape tarantula

---
+++ src/components/Tarantula/tools.js
@@ -0,0 +1,12 @@
+function degreesToRadians(degrees) {
+	return degrees * Math.PI / 180;
+}
+
+function radiansToDegrees(radians) {
+	return radians * 180 / Math.PI;
+}
+
+function legCounterAngle(bodyAngle, distanceFromAxisToLeg, distanceFromLegToFloor) {
+  const beta = radiansToDegrees(Math.asin(distanceFromAxisToLeg * Math.sin(degreesToRadians(bodyAngle)) / distanceFromLegToFloor))
+  return -1 * (bodyAngle + beta)
+}
