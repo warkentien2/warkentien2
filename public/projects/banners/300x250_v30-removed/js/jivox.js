d7712d6ae0cfe4fb4fa58d51e9b3189ddd309c5c
warkentien2 <philip.dw2@gmail.com>
2019-09-26 09:34:43 -0300

    Add changes

--- public/projects/banners/300x250_v30-removed/js/jivox.js
+++
@@ -1,6 +0,0 @@
-function initiate() {
-    var dynamicData = jvxAd.getDynamicData(), selectedRow = dynamicData.AllyBank[0];
-    document.querySelector("#frm1-bkg1").src = selectedRow.frm1Bkg1, document.querySelector("#frm1-bkg1-copy1").src = selectedRow.frm1Bkg1Copy, 
-    document.querySelector("#frm1-img1").src = selectedRow.frm1Img1, document.querySelector("#frm1-copy1").innerHTML = selectedRow.frm1Copy1, 
-    document.querySelector("#frm2-copy1").innerHTML = selectedRow.frm2Copy1, document.querySelector("#frm1-cta1").innerHTML = selectedRow.frm1Cta1 || "learn more";
-}
--- public/projects/banners/300x250_v30-removed/js/rateArray.js
+++
@@ -1,6 +0,0 @@
-var product = [ {
-    productCode: "CD",
-    termId: "Z-12",
-    min: "25000",
-    max: "9.9999999999E8"
-} ];
